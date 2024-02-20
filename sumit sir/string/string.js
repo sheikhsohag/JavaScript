72160f11da67a1286bfdfccbd66edf0ce8b3bdb3
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-20 16:34:37 +0600

    string and ==value check  === value and type

---
+++ sumit sir/string/string.js
@@ -0,0 +1,6 @@
+// let str = "ZAQWR'D'SXC CVFRTGBV NMJHYUIKLO";
+let str = 'ZAQWR"D"SXC CVFRTGBV NMJHYUIKLO';
+
+// '' use single qurte if "" use in inner text vice versa.
+
+console.log(str.length)
