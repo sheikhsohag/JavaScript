e0b9d170f71cf206ffe422401e27060cb21c96ba
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-21 15:02:13 +0600

    js Arrays

---
+++ sumit sir/array/array.js
@@ -0,0 +1,67 @@
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars)
+// console.log(typeof cars)
+// cars[1]="Audi"
+// console.log(cars)
+
+
+// // const Cars = new Array("Saab", "Volvo", "BMW");
+// // For simplicity, readability and execution speed, use the array literal method.
+
+// console.log(typeof Cars)
+// let strCars = cars.toString()
+// console.log(strCars)
+// console.log(typeof strCars)
+// // ======================================================
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let length = fruits.length;
+// console.log(length)
+
+// for (let i=0;i<fruits.length;i++)
+// {
+//     console.log(fruits[i]);
+// }
+
+// // ===================================================
+
+
+// fruits.forEach(myFunction);
+
+// function myFunction(value){
+//     console.log("yes", value)
+// }
+
+
+// console.log(fruits)
+// ===========================================================
+// const fruits = ["Banana", "Orange", "Apple"];
+// console.log(fruits)
+// fruits.push("Lemon");  // Adds a new element (Lemon) to fruits
+// console.log(fruits)
+
+// ============================================================
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits[6] = "Lemon";  // Creates undefined "holes" in fruits
+// console.log(fruits)
+
+// ===============================================================
+
+// let cars = []
+// cars["audi"] = 2;
+// cars["bmw"]=5;
+// cars["bmw"]++;
+// cars["bmw"]++;
+
+// console.log(cars["bmw"])
+
+// ===================================================
+
+const points = new Array(40);  
+console.log(points)
+
+const point = [40]
+
+console.log(point)
+console.log(Array.isArray(point))
+
