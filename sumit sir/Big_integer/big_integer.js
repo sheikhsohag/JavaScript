44956701cd599d1b1c48abf57b2e750382801257
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-21 11:40:49 +0600

    js Number method & more in w3school

---
+++ sumit sir/Big_integer/big_integer.js
@@ -0,0 +1,4 @@
+let x = 9999999999999999999999999999999n;
+let type = typeof x;
+console.log(type)
+console.log(x*x)
---
+++ sumit sir/int_method/method.js
@@ -0,0 +1,24 @@
+let x = 123;
+console.log(typeof x);
+
+let xx = x.toString()
+console.log(typeof xx)
+console.log(typeof x)
+
+let value = 1223455664566;
+//================================================
+
+let valuenew = value.toExponential(10);
+console.log(valuenew)
+
+//============================================
+
+let y = new Number(123);
+console.log(typeof y)
+
+// object convert into number
+
+let yy = y.valueOf();
+console.log(typeof yy)
+
+// =========================================
