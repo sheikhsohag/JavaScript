6f3635a18cf7c6b96d7061e84dd3a232a8d2e223
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-21 15:39:36 +0600

    array method of js

---
+++ sumit sir/array_method/method.js
@@ -0,0 +1,98 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits)
+// let str = fruits.toString()
+// console.log(str)
+// fruits.push("peyara");
+// str = fruits.toString()
+// console.log(str)
+// console.log(fruits)
+// ===============================================================
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits.at(-2);
+// console.log(fruit)
+
+
+// ===================================================================
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let val = fruits.join(" * ");
+
+// console.log(val)
+// console.log(typeof val)
+
+// for(let i =0; i<val.length; i++)
+// {
+//     console.log(val[i])
+// }
+
+// =======================================
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.pop();
+// console.log(fruits)
+
+
+// ====================================================
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift();
+
+
+
+// =============================================
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Lemon");
+
+// ================================================
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[0];
+
+// ==============================================
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// const myChildren = myGirls.concat(myBoys);
+// myGirls=myChildren
+// console.log(myGirls)
+
+// ==========================================================
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// let fruit = fruits.copyWithin(0, 0, 4);
+// console.log(fruit)
+
+// ===============================================
+
+
+// const myArr = [[1,2],[3,4],[5,6]];
+// const newArr = myArr.flat();
+// console.log(newArr)
+// console.log(typeof newArr)
+// console.log(Array.isArray(newArr))
+
+// =========================================
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "Lemon", "Kiwi");
+
+// console.log(fruits)
+// ==========================================
+
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// const spliced = months.toSpliced(4, 1, 'may', 'des');
+
+// console.log(spliced)
+
+
+// ==============================
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(3, 4);
+
+console.log(citrus, fruits)
