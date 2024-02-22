b96159ed8e1d32c311009c4061e0c56f18211a10
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-22 09:58:07 +0600

    array sort and iterate

---
+++ sumit sir/array_sort_function/sort.js
@@ -0,0 +1,73 @@
+// const numbers = [45, 4, 9, 16, 25];
+// let txt = "";
+// let num=numbers.forEach(myFunction);
+
+// function myFunction(value, index, array) {
+//   console.log(value, index, array);
+//   return value;
+// }
+
+// console.log(num);
+
+// =========================================================
+
+
+// const numbers1 = [45, 4, 9, 16, 25];
+// const numbers2 = numbers1.map(myFunction);
+
+// function myFunction(value, index, array) {
+//   console.log(value)
+// }
+
+// console.log(numbers2)
+
+
+// ==================================================
+
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduce(myFunction,100);
+
+// function myFunction(total, value) {
+//   return total + value;
+// }
+
+// console.log(sum)
+
+// =================================
+
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduceRight(myFunction);
+
+// function myFunction(total, value, index, array) {
+//     console.log(total)
+//   return total + value;
+// }
+// =================================================
+// let value = 12345;
+
+// value = value.toString();
+// console.log(typeof value)
+// console.log(value);
+
+// let array = Array.from(value)
+// console.log(array)
+
+// array.forEach(functions);
+
+// function functions(value)
+// {
+//     console.log(value)
+// }
+// ========================================
+
+let value = {}
+
+value['sohag'] = 2;
+value['tonmoy'] = 4;
+value['jalal'] = 5;
+console.log(value)
+
+let key = value.entries();
+console.log(key);
+
+console.log(typeof value)
