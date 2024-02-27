606a3ae0d2b6fc373d252611cc00b930bdb9664a
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-27 22:59:20 +0600

    object mutable & immutable

---
+++ w3school_sumitsir/js-object-details/object.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'sohag',
+    age: 35
+};
+
+person.name = "Bangladesh";
+// it is muttable partial change
+
+let num = 3;
+num = 4;
+// imuttable becasue of it is does not change it replace...
