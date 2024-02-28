54d9a8ccc9f0ef9c74154fd906bbaa6fd0de9f82
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-28 12:48:21 +0600

    object property

---
+++ w3school_sumitsir/js-object-details/property/propertice.js
@@ -0,0 +1,21 @@
+// const myObj = {
+//     name: "John",
+//     age: 30,
+//     cars: [
+//       {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+//       {name:"BMW", models:["320", "X3", "X5"]},
+//       {name:"Fiat", models:["500", "Panda"]}
+//     ]
+//   }
+
+//   for(let i in myObj.cars)
+//   {
+//     console.log(myObj.cars[i].models[0]);
+//   }
+
+
+// ==========================
+
+let a = new String("Bangladesh");
+
+console.dir(a);
