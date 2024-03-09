f2a231ffb5744581ee97b13cd5a86c2a773fae90
sheikhsohag <sheikhsohag000@gmail.com>
2024-03-09 21:05:14 +0600

    screen property

---
+++ w3school_sumitsir/BOM/screen/screen.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("height").innerHTML =
+    "Available Screen Width: " + window.location.href + "<br> Hostname: " + 
+    window.location.hostname;
+}
