74d1f56d1ff4016082a77906dd49eebe7acd2150
sheikhsohag <sheikhsohag000@gmail.com>
2024-03-10 23:04:02 +0600

    ajax introduction

---
+++ w3school_sumitsir/ajax/introduction/ajax.js
@@ -0,0 +1,12 @@
+function loadDoc() {
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML = this.responseText;
+      }
+
+    xhttp.open("GET", "ajax.txt");
+    
+    xhttp.send();
+  }
